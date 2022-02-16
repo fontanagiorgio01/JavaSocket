6ed3063d91f9bff9ec4685f5672ed4bddf008498
Fontana Giorgio <fontana.giorgio@osdbsesto>
2022-02-16 09:41:05 +0100

    fixed client

--- ClientServer/src/clientserver/ClientServer.java
+++ ClientServer/src/clientserver/ClientServer.java
@@ -1,8 +1,10 @@
 
 package clientserver;
 
-
-
+import java.net.*;
+import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class ClientServer {
 
@@ -10,8 +12,24 @@ public class ClientServer {
     
    
     public static void main(String[] args) {
-        
-    
+        try {
+            ServerSocket serverSocket = new ServerSocket(5000);
+            Socket clientSocket = serverSocket.accept();
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            String greeting = in.readLine();
+            if ("hello server".equals(greeting)) {
+                out.println("hello client");
+            } else {
+                out.println("unrecognised greeting");
+            }
+            in.close();
+            out.close();
+            clientSocket.close();
+            serverSocket.close();
+        } catch (IOException ex) {
+            Logger.getLogger(ClientServer.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
 }
--- JavaSocket/src/Server/Server.java
+++ JavaSocket/src/Server/Server.java
@@ -3,23 +3,32 @@ package Server;
 
 import java.net.*;
 import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 public class Server {
 
     
     public static void main(String[] args) {
-        Server serverSpcket = new Server();
-        clientSocket = serverSocket.accept();
-        out = new PrintWriter(clientSocket.getOutputStream(), true);
-        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-        String greeting = in.readLine();
+        try {
+            ServerSocket serverSocket = new ServerSocket(5000);
+            Socket clientSocket = serverSocket.accept();
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            String greeting = in.readLine();
             if ("hello server".equals(greeting)) {
                 out.println("hello client");
-            }
-            else {
+            } else {
                 out.println("unrecognised greeting");
             }
+            in.close();
+            out.close();
+            clientSocket.close();
+            serverSocket.close();
+        } catch (IOException ex) {
+            Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
 }
